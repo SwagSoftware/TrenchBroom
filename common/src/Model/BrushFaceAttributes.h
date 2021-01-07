2de9b9fdd434a4f79047c87f811ce7399062ffdf
Robert Beckebans <robert.beckebans@gmail.com>
2021-01-07 21:01:58 +0100

    More work with brush primitives 2

--- common/src/Model/BrushFaceAttributes.h
+++ common/src/Model/BrushFaceAttributes.h
@@ -107,6 +107,7 @@ namespace TrenchBroom {
                 return m_bpMode;
             }
             bool setBrushPrimitMatrix(const vm::mat4x4f& matrix);
+            const vm::mat4x4f& bpMatrix() const;
         };
     }
 }
